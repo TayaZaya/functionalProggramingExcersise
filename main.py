438d0369ff98124f64165f8db3fca1aea1c93167
tayu1994@gmail.com <TayaZaya1994>
2022-04-29 18:35:14 +0300

    done questions 2+4

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from functools import reduce
 my_pets = ['sisi', 'bibi', 'titi', 'carla']
 
 def item_capitalize(item):
-    return item.capitalize()
+    return item.upper()
 
 print(list(map(item_capitalize, my_pets)))
+
+
 
 #2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
 my_strings = ['a', 'b', 'c', 'd', 'e']
 my_numbers = [5,4,3,2,1]
+my_numbers.sort()
+print(list(zip(my_strings, my_numbers)))
+
 
 
 #3 Filter the scores that pass over 50%
@@ -20,4 +25,13 @@ def overthithty(item):
     return item > 50
 
 print(list(filter(overthithty, scores)))
+
+
+
 #4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+from functools import reduce
+
+def accumulator(acc, item):
+    return acc + item
+
+print(reduce(accumulator, (my_numbers + scores)))
